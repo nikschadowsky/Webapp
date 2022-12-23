51d1a062bac30cdfc8c06507913a38dacdcda416
Nik Schadowsky <n.s-schadowsky@t-online.de>
2022-12-23 11:36:58 +0100

    outro first iteration

--- src/components/section/Outro.vue
+++ src/components/section/Outro.vue
@@ -4,23 +4,50 @@ import MainSection from "../structure/MainSection.vue"
 import HeaderSection from "../structure/HeaderSection.vue"
 
 
-    export default{
+export default {
 
-components:{
-    MainSection,
-    HeaderSection
-}
-
+    components: {
+        MainSection,
+        HeaderSection
     }
+
+}
 
 </script>
 
 <template>
-    <HeaderSection>
-        Outro
-    </HeaderSection>
+    
+    <MainSection>
+        <div class="outro-parent flex">
+            <div class="outro-child " >
+                <p class="colored">Created with</p>
+                <br>
+                <p class="">
+                      vue 3 <br>
+                     vue-observe-visibility
+                </p>
+                
+                
+            </div>
+            <div class="outro-child">
+                <p class="colored">Kontakt</p>
+                nikschadowsky@nikschadowsky.de
+            </div>
+        </div>
+
+    </MainSection>
+
 </template>
 
 <style>
+    .outro-parent{
+        flex-wrap: wrap;
+    }
+.outro-child{
+    
+}
 
+    .outro-child:last-of-type {
+       margin-left: auto;
+    }
 </style>
