79f0f79314e3adcef6677a5b8efa8cb54ecc7fcf
Nik Schadowsky <n.s-schadowsky@t-online.de>
2023-02-06 20:58:53 +0100

    utilities module to host general functions

--- src/util.js
+++ src/util.js
@@ -1,4 +1,8 @@
 export function isMobile() {
     console.log(window.innerWidth);
     return (window.innerWidth <= 800);
-}   
+}
+
+export function getRandomInt(max){
+    return Math.floor(Math.random() * max)
+}
