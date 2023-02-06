c10dbdf7277c5a6327ffc482c4f0b4eae1e68556
Nik Schadowsky <n.s-schadowsky@t-online.de>
2023-02-06 20:59:24 +0100

    Placeholder About to test functionality

---
+++ src/components/sections/About.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default  {
+
+
+updated(){
+    console.log("about updated");
+}
+
+}
+
+</script>
+
+<template>
+    <div id="about" class="section">
+
+        About section
+
+    </div>
+</template>
+<style scoped>
+
+</style>
