b0f6b81555c136628ef01c65276567f75f02ab9b
Nik Schadowsky <n.s-schadowsky@t-online.de>
2023-02-07 10:56:18 +0100

    about section implemented

--- src/components/sections/About.vue
+++ src/components/sections/About.vue
@@ -1,11 +1,14 @@
 <script>
 
-export default  {
+export default {
+
+    methods: {
+        scrollToProjects() {
+            document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+        }
+    }
 
 
-updated(){
-    console.log("about updated");
-}
 
 }
 
@@ -14,10 +17,57 @@ updated(){
 <template>
     <div id="about" class="section">
 
-        About section
+        <div id="text-block">
+            <div id="text">
+                <p>
+                    Heyho! <br>
+                    <br>Ich bin Nik Schadowsky.
+                    Seit August 2022 Dualer Student für Angewandte Informatik bei <a class="link colored"
+                        href="https://www.tk.de/" target="_blank" rel="noopener noreferrer">@Techniker Krankenkasse</a>.
+                    <br><br>
 
+                    In meiner Freizeit arbeite ich viel mit Java, aber auch mit HTML, JavaScript, CSS und gelegentlich
+                    Python.
+                    Mehr dazu in meinen <span class="colored link" @click="scrollToProjects"> Projekten</span>.
+                </p>
+                <div id="img-holder">
+                    <img id="picture" src="../../image_about.png" alt="image of Nik Schadowsky">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 <style scoped>
+#text-block {
+    display: flex;
+    flex-direction: row;
 
+
+    max-width: 1000px;
+    max-height: 500px
+}
+
+#text {
+    height: 100%;
+    display: flex;
+    align-items: center;
+}
+
+#img-holder {
+    
+    border: 1px var(--accent-color) solid;
+    padding: 10px;
+
+    margin: 0 50px ;
+}
+
+#picture {
+    height: 100%;
+    width: 100%;
+    display: block;
+
+    border: 1px solid rgba(104, 131, 59, .5);
+
+    margin: 0;
+}
 </style>
