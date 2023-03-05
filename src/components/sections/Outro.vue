4f2dd966caa496448b0348e61ed2792645c76aaa
nikschadowsky <n.s-schadowsky@t-online.de>
2023-03-05 18:49:21 +0100

    outro css

--- src/components/sections/Outro.vue
+++ src/components/sections/Outro.vue
@@ -4,20 +4,20 @@
     <div class="section">
 
         <div class="flexbox-table">
-            <div id="made-with-panel" class="debug">links</div>
-            
+            <div id="made-with-panel" class="debug">
+                <p class="comment">Made with</p>
+
+            </div>
+
             <div id="contact-panel" class="debug">rechts</div>
-            
+
         </div>
 
     </div>
-
-
-
 </template>
 
 <style scoped>
-.flexbox-table{
+.flexbox-table {
     display: flex;
 
     width: 500px;
@@ -25,11 +25,8 @@
     justify-content: space-between;
 
 }
-#made-with-panel {
 
-}
-#contact-panel {
+#made-with-panel {}
 
-}
-
+#contact-panel {}
 </style>
