c577855c3235b686ecaf0f238b88819032d23fac
Nik Schadowsky <n.s-schadowsky@t-online.de>
2023-03-26 13:05:57 +0200

    SVG Icons extracted

---
+++ src/components/elements/LetterIcon.vue
@@ -0,0 +1,7 @@
+<template>
+    <svg  width="800px" height="800px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
+        <title> Letter</title>
+        <path
+            d="M29,11H3a1,1,0,0,0-1,1V29a1,1,0,0,0,1,1H29a1,1,0,0,0,1-1V12A1,1,0,0,0,29,11Zm-2.79,2L16,20.23,5.79,13ZM4,28V14.18l11.42,8.09a1,1,0,0,0,1.16,0L28,14.18V28Z" />
+    </svg>
+</template>
