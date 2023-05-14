6bfc7d04bd5f71274dc73ccf3f48b951c051d61f
Nik Schadowsky <n.s-schadowsky@t-online.de>
2023-05-14 13:21:57 +0200

    created base components for designing

---
+++ src/components/elements/RoundedSquare.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    props: {
+        imageURL: {
+            type: String,
+            default: ""
+        },
+        objectID: String,
+        objectClass: String,
+    }
+}
+</script>
+<template>
+    <svg :class="objectClass" :id="objectID" width="200" height="200" viewBox="0 0 100 100">
+        <defs>
+            <path id="objPath"
+                d="M20 0 L80 0 A 20 20 270 0 1 100 20 L100 80 A 20 20 0 0 1 80 100 L20 100 A 20 20 90 0 1 0 80 L 0 20 A 20 20 180 0 1 20 0z">
+            </path>
+            <clipPath id="cp" >
+                <use href="#objPath"></use>
+            </clipPath>
+        </defs>
+        <use href="#objPath" fill="#fff"/>
+        <image v-if="imageURL.length > 0" x="0" y="0" width="100" height="100" :href="imageURL"
+            preserveAspectRation="xMidYMid meet" clip-path="url(#cp)"></image>
+    </svg>
+</template>
+
+<style scoped></style>
---
+++ src/components/elements/RoundedTriangle.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    props: {
+        objectID: String,
+        objectClass: String
+    }
+}
+</script>
+<template>
+    <svg width="100" height="100" viewport="0 0 100 100">
+        <path fill="#fff" stroke-width="1" stroke="#fff"
+             d="M20 0 h60
+              A 20 20 0 0 1 97.321 30
+              l-30 51.961
+              a 20 20 120 0 1 -34.642 0
+              L 2.679 30
+              A 20 20 240 0 1 20 0">
+         </path>
+    </svg>
+</template>
+
+<style scoped></style>
