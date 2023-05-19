ad49dcb87d7ce8def18fb4d7e3d467903bee32c2
Nik Schadowsky <n.s-schadowsky@t-online.de>
2023-05-19 21:54:50 +0200

    fixed issues and unnecessary code objclass and id

--- src/components/elements/RoundedSquare.vue
+++ src/components/elements/RoundedSquare.vue
@@ -11,7 +11,7 @@ export default {
 }
 </script>
 <template>
-    <svg :class="objectClass" :id="objectID" width="200" height="200" viewBox="0 0 100 100">
+    <svg width="200" height="200" viewBox="0 0 100 100">
         <defs>
             <path id="objPath"
                 d="M20 0 L80 0 A 20 20 270 0 1 100 20 L100 80 A 20 20 0 0 1 80 100 L20 100 A 20 20 90 0 1 0 80 L 0 20 A 20 20 180 0 1 20 0z">
@@ -20,7 +20,7 @@ export default {
                 <use href="#objPath"></use>
             </clipPath>
         </defs>
-        <use href="#objPath" fill="#fff"/>
+        <use href="#objPath"/>
         <image v-if="imageURL.length > 0" x="0" y="0" width="100" height="100" :href="imageURL"
             preserveAspectRation="xMidYMid meet" clip-path="url(#cp)"></image>
     </svg>
--- src/components/elements/RoundedTriangle.vue
+++ src/components/elements/RoundedTriangle.vue
@@ -7,8 +7,8 @@ export default {
 }
 </script>
 <template>
-    <svg width="100" height="100" viewport="0 0 100 100">
-        <path fill="#fff" stroke-width="1" stroke="#fff"
+    <svg fill="#000000" width="120" height="120" viewport="0 0 100 100">
+        <path id="path"
              d="M20 0 h60
               A 20 20 0 0 1 97.321 30
               l-30 51.961
@@ -19,4 +19,13 @@ export default {
     </svg>
 </template>
 
-<style scoped></style>
+<style scoped>
+#path{
+    translate: 10px 17px;
+    
+}
+
+svg{
+    transform-origin: 50% calc(33% + 16px);
+}
+</style>
